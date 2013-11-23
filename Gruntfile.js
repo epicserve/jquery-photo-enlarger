3c4f7e6b0747a20cbc9a0a15a502f325b9879502
Brent O'Connor <epicserve@gmail.com>
2013-11-22 17:19:11 -0800

    Made sure scripts pass JSHint tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,35 @@
+
+'use strict';
+
 module.exports = function(grunt) {
 
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
+    // Project configuration.
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    githubPages: {
-      target: {
-        options: {
-          // The default commit message for the gh-pages branch
-          commitMessage: 'Github pages update'
+        githubPages: {
+            target: {
+                options: {
+                    // The default commit message for the gh-pages branch
+                    commitMessage: 'Github pages update'
+                },
+                // The folder where your gh-pages repo is
+                src: '_site'
+            }
         },
-        // The folder where your gh-pages repo is
-        src: '_site'
-      }
-    },
 
-    copy: {
-      gh_pages: {expand: true, cwd: 'example/', src: ['**'], dest: '_site/'}
-    }
+        copy: {
+            gh_pages: {expand: true, cwd: 'example/', src: ['**'], dest: '_site/'}
+        }
 
-  });
+    });
 
-  grunt.registerTask('deploy', ['copy:gh_pages', 'githubPages:target']);
+    grunt.registerTask('deploy', ['copy:gh_pages', 'githubPages:target']);
 
-  grunt.registerTask('default', []);
+    grunt.registerTask('default', []);
 
 };
