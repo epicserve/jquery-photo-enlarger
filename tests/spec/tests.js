6b6d9b376d37e54051e7decc6c15d2141968a8b5
Brent O'Connor <epicserve@gmail.com>
2013-11-28 01:05:15 -0800

    Added some more tests

--- tests/spec/tests.js
+++ tests/spec/tests.js
@@ -13,11 +13,41 @@ var assert = assert || {},
         describe('First small thumb', function() {
 
             it('should have click a event bound to it', function() {
-                var $firstImg = $('.thumb img:first'),
+                var $firstImg = $('.thumb:first img:first'),
                     _data = $._data($firstImg[0]);
                 expect(_data.events).to.have.property('click');
                 expect(_data.events.click).to.be.an('Array');
                 expect(_data.events.click).to.have.length(1);
+            });
+
+        });
+
+        describe('First thumb enlarged', function() {
+
+            it('should have a click event bound to the large image and icon', function(done) {
+
+                $('.thumb:first img:first').click();
+                setTimeout(function() {
+                    var $thumb = $('.thumb:first'),
+                        $large_img = $thumb.find('.thumb-large:first img:first'),
+                        $icon = $thumb.find('.thumb-large:first .state-icon:first'),
+                        _data = $._data($large_img[0]),
+                        _icon_data = $._data($icon[0]);
+
+                    // check large image data
+                    expect(_data.events).to.have.property('click');
+                    expect(_data.events.click).to.be.an('Array');
+                    expect(_data.events.click).to.have.length(1);
+
+                    // check icon data
+                    expect(_icon_data.events).to.have.property('click');
+                    expect(_icon_data.events.click).to.be.an('Array');
+                    expect(_icon_data.events.click).to.have.length(1);
+
+                    $large_img.click();
+                    done();
+
+                }, 300);
             });
 
         });
